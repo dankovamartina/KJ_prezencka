82fc1d6f4801811727712aa810f67936f5dc1e7f
Martina Daňková <andorianka@gmail.com>
2022-03-28 08:50:08 +0200

    change check for whole number to check for natural number

--- analyse%numbers2.py
+++ analyse%numbers2.py
@@ -1,16 +1,18 @@
-def is_whole_number(number):
+def is_natural_number(number):
     """
     overuje, zda je zadane cislo cele cislo
     :param number: cislom ktere chceme proverit
     :return: (bool) True / cislo je cele, False / cislo neni cele
     """
 
-    bool = isinstance(number, int)
-    return bool
+    if isinstance(number, int) == True and number > 0:
+        return True
+    else:
+        return False
 
 def main ():
     my_number = 7
-    print(f"{my_number} je cele: {is_whole_number(my_number)}")
+    print(f"{my_number} je prirozene: {is_natural_number(my_number)}")
 
 
 if __name__ == "__main__":
