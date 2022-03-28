34c9524dacb3f90e089ada6e64a38210fe010141
Martina Daňková <andorianka@gmail.com>
2022-03-28 08:59:27 +0200

    rozsireni funkce o urceni sudosti cisla

--- analyse%numbers2.py
+++ analyse%numbers2.py
@@ -10,9 +10,22 @@ def is_natural_number(number):
     else:
         return False
 
+def is_even_number(number):
+    """
+    proveri, zda je cislo sude nebo liche
+    :param number: cislo ktere proverujeme
+    :return: (bool) True = sude, False = liche
+    """
+
+    if number % 2 == 0:
+        return True
+    else:
+        return False
+
 def main ():
     my_number = 7
     print(f"{my_number} je prirozene: {is_natural_number(my_number)}")
+    print (f"{my_number} je sude {is_even_number(my_number)}")
 
 
 if __name__ == "__main__":
